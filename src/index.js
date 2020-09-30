035460ceb0e225f62b11469cbc0a9c1e41196910
Alex Valynets <sjabruxa@gmail.com>
2020-09-30 10:00:09 +0300

    init: start typical array problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+exports.min = function min(array) {
+    if (!array || array.length == 0) return 0;
+    return array.reduce((min, value) => (value < min ? value : min));
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length == 0) return 0;
+    return array.reduce((max, value) => (value > max ? value : max));
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length == 0) return 0;
+    return array.reduce((avg, value) => avg + value) / array.length;
+};
